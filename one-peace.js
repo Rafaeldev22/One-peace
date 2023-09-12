c0714f2cdf06cd1a8a0eedfbd81a7269d7466117
RAFAEL MENDONÇA DE MEDEIROS <RAFAEL6043@DIGITALCOLLEGE.LOCAL>
2023-09-12 19:15:01 -0300

    Projeto Quase Finalizado

--- one-peace.js
+++ one-peace.js
@@ -1,6 +1,9 @@
 const imgZoro = document.getElementById("img-zoro");
 const imgChopper = document.getElementById("img-chopper");
 const imgLuffy = document.getElementById("img-luffy");
+const imgUsop = document.getElementById("img-usop");
+const imgBrook = document.getElementById("img-brook");
+
 
 imgZoro.addEventListener("click", () =>{
     zoro.style.display='flex';
@@ -24,4 +27,20 @@ imgLuffy.addEventListener("click", () =>{
     luffy.style.display='flex';
     usop.style.display='none';
     brook.style.display='none';
+})
+
+imgUsop.addEventListener("click", () =>{
+    zoro.style.display='none';
+    tonyChopper.style.display='none';
+    luffy.style.display='none';
+    usop.style.display='flex';
+    brook.style.display='none';
+})
+
+imgBrook.addEventListener("click", () =>{
+    zoro.style.display='none';
+    tonyChopper.style.display='none';
+    luffy.style.display='none';
+    usop.style.display='none';
+    brook.style.display='flex';
 })
